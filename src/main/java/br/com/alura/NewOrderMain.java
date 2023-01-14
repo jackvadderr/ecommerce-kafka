07c1396d4e508f5e5b3bae8575216c22bd38a494
jackvadderr <jacjacckck@gmail.com>
2023-01-13 21:00:34 -0700

    Vários consumidores e produtores

--- src/main/java/br/com/alura/NewOrderMain.java
+++ src/main/java/br/com/alura/NewOrderMain.java
@@ -1,5 +1,6 @@
 package br.com.alura;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -11,15 +12,19 @@ import java.util.Properties;
 public class NewOrderMain {
     public static void main(String[] args)throws Exception {
         var producer = new KafkaProducer<String, String>(properties());
-        var value = "132123,67523,7894589745";
+        var value = "132123,67523,1234589745";
         var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, (data, ex) -> {
-            if(ex != null){
+        Callback callback = (data, ex) -> {
+            if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
             System.out.println("SUCESSO ENVIANDO " + data.topic() + ":::" + data.partition() + "/ offset" + data.offset() + "/" + data.timestamp());
-        }).get();
+        };
+        var email = "Thank you for your order! We are processing your order!";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        producer.send(record, callback).get();
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties properties() {
