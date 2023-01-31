2a12b7b694f1af1e39a3375f824622ec44a661a6
jackvadderr <jacjacckck@gmail.com>
2023-01-30 18:32:49 -0700

    Deserializacao customizada

--- src/main/java/br/com/alura/FraudDetectorService.java
+++ src/main/java/br/com/alura/FraudDetectorService.java
@@ -13,14 +13,15 @@ import java.util.UUID;
 public class FraudDetectorService {
     public static void main(String[] args)throws Exception {
         var fraudService = new FraudDetectorService();
-        try(var service = new KafkaService(FraudDetectorService.class.getSimpleName(),
+        try(var service = new KafkaService<>(FraudDetectorService.class.getSimpleName(),
                 "ECOMMERCE_NEW_ORDER",
-                fraudService::parse)) {
+                fraudService::parse,
+                Order.class)) {
             service.run();
         }
     }
 
-    private void parse(ConsumerRecord<String, String> record) {
+    private void parse(ConsumerRecord<String, Order> record) {
         System.out.println("----------------------------------------");
         System.out.println("Processing new order, checking for fraud");
         System.out.println(record.key());
--- src/main/java/br/com/alura/KafkaService.java
+++ src/main/java/br/com/alura/KafkaService.java
@@ -1,6 +1,7 @@
 package br.com.alura;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
@@ -11,23 +12,23 @@ import java.util.Properties;
 import java.util.UUID;
 import java.util.regex.Pattern;
 
-class KafkaService implements Closeable {
-    private final KafkaConsumer<String, String> consumer;
+class KafkaService<T> implements Closeable {
+    private final KafkaConsumer<String, T> consumer;
     private final ConsumerFunction parse;
 
-    KafkaService(String groupId, String topic, ConsumerFunction parse) {
-        this(parse, groupId);
+    KafkaService(String groupId, String topic, ConsumerFunction parse, Class<T> type) {
+        this(parse, groupId, type);
         consumer.subscribe(Collections.singletonList(topic));
     }
 
-    KafkaService(String groupId, Pattern topic, ConsumerFunction parse) {
-        this(parse, groupId);
+    KafkaService(String groupId, Pattern topic, ConsumerFunction parse, Class<T> type) {
+        this(parse, groupId, type);
         consumer.subscribe(topic);
     }
 
-    private KafkaService(ConsumerFunction parse, String groupId) {
+    private KafkaService(ConsumerFunction parse, String groupId, Class<T> type) {
         this.parse = parse;
-        this.consumer = new KafkaConsumer<>(properties(groupId));
+        this.consumer = new KafkaConsumer<>(properties(type, groupId));
     }
 
     void run() {
@@ -35,20 +36,21 @@ class KafkaService implements Closeable {
             var records = consumer.poll(Duration.ofMillis(100));
             if (!records.isEmpty()) {
                 System.out.println("Encontrei " + records.count() + " registros.");
-                for (var record : records) {
+                for (ConsumerRecord<String, T> record : records) {
                     parse.consume(record);
                 }
             }
         }
     }
 
-    static Properties properties(String groupId) {
+    private Properties properties(Class<T> type, String groupId) {
         var properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GsonDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
+        properties.setProperty(GsonDeserializer.TYPE_CONFIG, type.getName());
         return properties;
     }
 
